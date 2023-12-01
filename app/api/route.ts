b5bd8fde8c0b20e80406e9cd6c13694c1afbb3f6
AWKohler <aronne336@icloud.com>
2023-12-01 15:06:24 -0500

    fixed

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-// import fetch from 'node-fetch';
-
-export default async (req, res) => {
-    const response = await fetch('https://raw.githubusercontent.com/AWKohler/test-resources/main/embedai.js');
-    const scriptContent = await response.text();
-
-    res.setHeader('Content-Type', 'application/javascript');
-    res.send(scriptContent);
-};
