a4c7833bd771da2e882fca73f973c668becd6fef
AWKohler <aronne336@icloud.com>
2024-01-30 08:38:03 -0500

    Text effect, might remove later, new font

--- components/page/aboutMe.tsx
+++ components/page/aboutMe.tsx
@@ -1,10 +1,20 @@
 "use client"
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import React from "react";
+import React, {useRef} from "react";
 import {Separator} from "@/components/ui/separator";
 import {Button} from "@/components/ui/button";
 import Image from "next/image";
 import {GraduationCap, MapPin, School2} from "lucide-react";
+import {
+    motion,
+    useScroll,
+    useSpring,
+    useTransform,
+    useMotionValue,
+    useVelocity,
+    useAnimationFrame,
+} from "framer-motion";
+import {wrap} from "@motionone/utils";
 
 export default function AboutMe() {
 
@@ -18,8 +28,52 @@ export default function AboutMe() {
         window.location.href = 'https://www.instagram.com/aronnekohler/';
     };
 
+    const baseX = useMotionValue(0);
+    const { scrollY } = useScroll();
+    const scrollVelocity = useVelocity(scrollY);
+    const smoothVelocity = useSpring(scrollVelocity, {
+        damping: 50,
+        stiffness: 400,
+    });
+    const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+        clamp: false,
+    });
+
+    /**
+     * This is a magic wrapping for the length of the text - you
+     * have to replace for wrapping that works for you or dynamically
+     * calculate
+     */
+    const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+    const directionFactor = useRef<number>(1);
+
+    useAnimationFrame((t, delta) => {
+        // let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+        let moveBy = directionFactor.current * 100 * (delta / 1000);
+
+        /**
+         * This is what changes the direction of the scroll once we
+         * switch scrolling directions.
+         */
+        if (velocityFactor.get() < 0) {
+            directionFactor.current = -1;
+        } else if (velocityFactor.get() > 0) {
+            directionFactor.current = 1;
+        }
+
+        moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+        baseX.set(baseX.get() + moveBy);
+    });
+
     return (
         <section className="flex flex-col w-full py-12 md:py-24 lg:py-32 xl:py-48 rounded-3xl bg-card justify-center items-center">
+
+            <div className={"pb-14 text-7xl font-bold tracking-tighter flex items-center justify-center flex-col"}>
+                About Me
+            </div>
+
             <div className={"w-3/4 flex flex-row"}>
                 <div className={"p-16"}>
                     {/*<Image className={"rounded-full border-secondary border-2"} fill src={"/pfp.png"} alt={"Profile"}></Image>*/}
